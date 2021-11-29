1b009dd87ff59ac2467c63168aba4e96182351da
vitaxa <vitaxa93gamebox@gmail.com>
2021-11-29 16:19:48 +0300

    #major (#6)
    bump tag version

--- src/main/kotlin/com/rbkmoney/adapter/payout/test/client/RemoteClientImpl.kt
+++ src/main/kotlin/com/rbkmoney/adapter/payout/test/client/RemoteClientImpl.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.rbkmoney.adapter.payout.test.client.model.BaseResponse
 import com.rbkmoney.adapter.payout.test.client.model.MoneyTransferRequest
 import com.rbkmoney.adapter.payout.test.client.model.StatusRequest
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
@@ -14,6 +15,8 @@ class RemoteClientImpl(
     private val objectMapper: ObjectMapper,
     @Value("\${adapter.url}") private val basePath: String
 ) : RemoteClient {
+
+    private val log = KotlinLogging.logger { }
 
     override fun moneyTransfer(request: MoneyTransferRequest): BaseResponse {
         TODO("Not yet implemented")
